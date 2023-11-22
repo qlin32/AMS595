b593bfa9a876a5dcc379146b87df51c290cd0aa2
Qihao Lin <qihao.lin@stonybrook.edu>
2023-11-21 23:59:26 -0500

    change

--- CPPPROJECT1/cpphw1q3.cpp
+++ CPPPROJECT1/cpphw1q3.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 using namespace std;
 
+//question 3
 int main() {
+    //initiate all the variables
     int f1 = 1;
     int f2 = 1;
     int ff = 1;
 
+    //calculates all fibonacci numbers
     while (ff <= 4000000){
         cout << ff << ",";
         f1 = f2;
